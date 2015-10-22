30a341fb3d6b5fbcfe402e7011cef2fa066b4474
FreshRamen <al.tabq@yahoo.com>
2015-10-22 15:51:29 +0200

    Improved explanations

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,22 @@
 #### Programming Assignment 2 solution
 
-# ## Example usage:
-# # Create a large, square example matrix
-# mat.new <- matrix(sample(1:10^4, size = 1024), ncol = 32)
-# # Feed this into makeCacheMatrix() and store myMat
-# myMat <- makeCacheMatrix(mat.new)
-# myMat$get() # returns the matrix
-# cacheSolve(myMat) # returns and caches the inverse
-# myMat$getinv() # returns the cached inverse
+### This script contains two functions:
 
+## makeCacheMatrix() 
+# creates a special "matrix" object that can cache its inverse.
+# it returns a list of four functions (see inside)
+
+## cacheSolve() 
+# computes the inverse of the special "matrix" returned by makeCacheMatrix 
+# returns and caches the inverse
+
+## Detailed explanations are provided within the function
+
+## Below the functions I also added an example that shows how the functions work
 
 makeCacheMatrix <- function(mat = matrix()) {
 	## Function creates a special "matrix" object that can cache its inverse.
-	## returns a list: 
+	## returns a list of four functions: 
 	# set() to set the value of the matrix
 	# get() to get the value of the matrix
 	# setinv() to cache the value of the inverse matrix
@@ -41,3 +45,13 @@ cacheSolve <- function(mat, ...) {
 	mat$setinv(mat.inv)
 	mat.inv
 }
+
+
+# ## Example usage:
+# # Create a large, square example matrix
+# mat.new <- matrix(sample(1:10^4, size = 1024), ncol = 32)
+# # Feed this into makeCacheMatrix() and store myMat
+# myMat <- makeCacheMatrix(mat.new)
+# myMat$get() # returns the matrix
+# cacheSolve(myMat) # returns and caches the inverse
+# myMat$getinv() # returns the cached inverse
